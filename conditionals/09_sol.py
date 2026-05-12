01dee6ed8ae4b2dd5ebc8c0a7b92cdb9ad5b385e
VikasMain_git91 <vp9198776636@gmail.com>
2026-05-12 16:13:02 +0530

    fixing some errors

--- conditionals/09_sol.py
+++ conditionals/09_sol.py
@@ -1,6 +1,6 @@
 year = 2026
 
 if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
-    print(year , " is a leap year")
+    print(year , "is a leap year")
 else:
-    print(year , " is NOT a leap year")
+    print(year , "is NOT a leap year")
